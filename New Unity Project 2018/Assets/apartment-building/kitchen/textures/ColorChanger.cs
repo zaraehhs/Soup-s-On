284dbaedc8c32f78437e755a5de9899dba74c898
jungtack <justinbyun@gmail.com>
2020-02-26 11:37:06 -0500

    Justin Changes
    Justin Changes

---
+++ New Unity Project 2018/Assets/apartment-building/kitchen/textures/ColorChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public void changeColor();
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject dynamicColorObject in DynamicColorObject.list)
+            dynamicColorObject.GetComponent<Renderer>().sharedMaterial.color = Color.red;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New Unity Project 2018/Assets/apartment-building/kitchen/textures/DynamicColorObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DynamicColorObject : MonoBehaviour
+{
+    public static List<GameObject> list = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        list.Add(gameObject);
+        
+    }
+
+    // Update is called once per frame
+    void OnDestroy()
+    {
+        list.Remove(gameObject);
+    }
+}
