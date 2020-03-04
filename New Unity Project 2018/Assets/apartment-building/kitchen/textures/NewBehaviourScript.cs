db64fe3a1a11ed98d573ff0ff7d7e4cdcf91ae35
Zara Ehsan <zarae@live.unc.edu>
2020-03-04 01:42:55 -0500

    Added More assets and added Drag and Drop to all vegetables

--- New Unity Project 2018/Assets/apartment-building/kitchen/textures/NewBehaviourScript.cs
+++ New Unity Project 2018/Assets/apartment-building/kitchen/textures/NewBehaviourScript.cs
@@ -1,24 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class ColorRed : MonoBehaviour
-{
-    public GameObject plate7;
-    // Start is called before the first frame update
-    void Start()
-    {
-        void onClick()
-        {
-            plate7.GetComponent<Renderer>().sharedMaterial.color = Color.red;
-        }
+// public class ColorRed : MonoBehaviour
+// {
+//     public GameObject plate7;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         void onClick()
+//         {
+//             plate7.GetComponent<Renderer>().sharedMaterial.color = Color.red;
+//         }
         
-    }
+//     }
 
-    // Update is called once per frame
-    void Update()
-    {
+//     // Update is called once per frame
+//     void Update()
+//     {
         
-    }
-}
+//     }
+// }
