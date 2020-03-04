499866217d6f02aa65f8d40942afb2f73f71ed99
jackhen124 <hensleyjacka@gmail.com>
2020-03-03 20:46:51 -0500

    physics on origDragObject are now working

--- New Unity Project 2018/Assets/origDragObject.cs
+++ New Unity Project 2018/Assets/origDragObject.cs
@@ -21,15 +21,19 @@ public class origDragObject : MonoBehaviour
     private bool collide = false;
     private bool grabbed = false;
     private Vector3 newLocation;
+    private float maxDrag = 20;
     Rigidbody rb;
 
     //public GameObject : Transform;
     void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
         yStart = transform.position.y;
         xStart = transform.position.x;
         zStart = transform.position.z;
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+        rb.drag = 2;
     }
     
     private void Update()
@@ -39,8 +43,8 @@ public class origDragObject : MonoBehaviour
             rb.useGravity = false;
 
 
-            newLocation = GetMouseAsWorldPoint()+mOffset;
-            //newLocation = GetMouseAsWorldPoint();// + mOffset;
+            newLocation = GetMouseAsWorldPoint() +mOffset;
+            
 
             
             GameObject pot = GameObject.Find("pot");
@@ -49,27 +53,66 @@ public class origDragObject : MonoBehaviour
             Vector3 potPosition = potTransform.position;
             Debug.Log("position = " + transform.position);
             Debug.Log("target position = " + newLocation);
-           
+
             //Debug.Log("pot position = " + potPosition);
 
-
-            //float disFromStart = System.Math.Abs(xStart - potPosition.x);
-            //float disTotalToPot = System.Math.Abs(xStart - potPosition.x);
-            //float percentToPot = disFromStart / disTotalToPot;
+            
+            float disFromStart = System.Math.Abs(xStart - transform.position.x);
+            float xDisTotalToPot = System.Math.Abs(xStart - potPosition.x);
+            float percentToPot = disFromStart / xDisTotalToPot;
+            if (percentToPot > 1)
+            {
+                percentToPot = 1;
+            }
             //Debug.Log("ptp");
             //Debug.Log(percentToPot);
+            float zDisTotalToPot = System.Math.Abs(zStart - potPosition.z);
+            if (zStart > potPosition.z)
+            {
+                newLocation.z = zStart - zDisTotalToPot * percentToPot;
+            }
+            else
+            {
+                newLocation.z = zStart + zDisTotalToPot * percentToPot;
+            }
 
-            newLocation.z = potPosition.z;
-            
-            //Vector3 direction = Vector3.MoveTowards(transform.position, newLocation, 1);
-
-            Vector3 direction = new Vector3(newLocation.x - transform.position.x, newLocation.y - transform.position.y, potPosition.z).normalized;
-            //direction = transform.TransformDirection(direction);
            
+            Vector3 direction = new Vector3(newLocation.x - transform.position.x, newLocation.y - transform.position.y, newLocation.z - transform.position.z);
+            
+            direction = direction.normalized;
             
             Debug.DrawRay(transform.position, direction, Color.green);
-            //Debug.DrawRay(transform.position, newLocation,Color.green);
-            rb.AddForce(direction*100);
+
+            float xDis = System.Math.Abs(newLocation.x - transform.position.x);
+            float yDis = System.Math.Abs(newLocation.y - transform.position.y);
+            float zDis = System.Math.Abs(newLocation.z - transform.position.z);
+            float smoothDis = 1;
+            
+            float smoothx = 1;
+            float smoothy = 1;
+            float smoothz = 1;
+            
+            if (xDis < smoothDis)
+            {
+                smoothx = xDis / smoothDis;
+            }
+            if (yDis < smoothDis)
+            {
+                smoothy = yDis / smoothDis;
+            }
+            if (zDis < smoothDis)
+            {
+                smoothz = zDis / smoothDis;
+            }
+            direction.x *= smoothx;
+            direction.y *= smoothy;
+            direction.z *= smoothz;
+            //rb.AddForce(direction*80);
+            
+            rb.velocity = direction * 15;
+            
+            
+            Debug.Log(rb.velocity);
             /*
             Vector3 direction = Vector3.MoveTowards(transform.position, newLocation, step);
             float force = 10;
@@ -111,9 +154,9 @@ public class origDragObject : MonoBehaviour
     {
 
         // Pixel coordinates of mouse (x,y)
-
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         Vector3 mousePos = Input.mousePosition;
-        mousePos.z = transform.position.z;
+        mousePos.z = mZCoord;
         float mousey = Camera.main.ScreenToWorldPoint(mousePos).y;
         float mousex = Camera.main.ScreenToWorldPoint(mousePos).x;
         return new Vector3(mousex, mousey, transform.position.z);
@@ -144,16 +187,47 @@ public class origDragObject : MonoBehaviour
         //Debug.Log(percentToPot);
 
         newLocation.z = potPosition.z;
-        
+
+        double smooth = 0.01;
+            bool smoothx = false;
+            bool smoothy = false;
+            bool smoothz = false;
+            if (System.Math.Abs(newLocation.x - transform.position.x) < smooth)
+            {
+                Debug.Log("smooth x");
+                smoothx = true;
+            }
+            if (System.Math.Abs(newLocation.y - transform.position.y) < smooth)
+            {
+                Debug.Log("smooth y");
+                smoothy = true;
+            }
+            if (System.Math.Abs(newLocation.z - transform.position.z) < smooth)
+            {
+                Debug.Log("smooth z");
+                smoothz = true;
+            }
+        if (smoothx)
+            {
+                direction.x = 0;
+            }
+            if (smoothy)
+            {
+                direction.y = 0;
+            }
+            if (smoothz)
+            {
+                direction.z = 0;
+            }
         */
 
 
         //transform.position = newLocation;
         //}
-        
-        
 
-      
+
+
+
 
     }
 
