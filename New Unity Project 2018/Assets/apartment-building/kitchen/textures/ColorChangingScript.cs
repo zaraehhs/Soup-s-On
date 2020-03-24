d5ff5d1568e8d70b9857499b38db46b25f95ad4f
jungtack <justinbyun@gmail.com>
2020-03-24 17:18:22 -0400

    nlegh

---
+++ New Unity Project 2018/Assets/apartment-building/kitchen/textures/ColorChangingScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    //Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            rend.material.SetColor("_Color", Random.ColorHSV());
+        }
+    }
+}
---
+++ New Unity Project 2018/Assets/apartment-building/kitchen/textures/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            rend.material.SetColor("_Color", Random.ColorHSV());
+        }
+    }
+}
