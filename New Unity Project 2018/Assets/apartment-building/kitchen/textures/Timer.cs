dd69e42747c11a573c0d5227007c85c928f12de8
jungtack <justinbyun@gmail.com>
2020-03-30 18:45:40 -0400

    Timer changes

---
+++ New Unity Project 2018/Assets/apartment-building/kitchen/textures/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    //This script should be attached to a UI Text element
+    public Text counterText;
+    public float seconds, minutes;
+
+    void Start ()
+    {
+        counterText = GetComponent<Text>() as Text;
+    }
+    //call this on update
+    void Update ()
+	{
+        minutes = (int)(Time.time/60f);
+        seconds = (int)(Time.time % 60f);
+        counterText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+	}
+
+}
