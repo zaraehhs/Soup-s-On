45d8704ba9970f397cd99f99c7212d3f3f8a3e1c
jackhen124 <hensleyjacka@gmail.com>
2020-04-14 18:21:16 -0400

    backup

--- New Unity Project 2018/Assets/game.cs
+++ New Unity Project 2018/Assets/game.cs
@@ -29,14 +29,24 @@ public class game : MonoBehaviour
     public int carrotCount;
     public int potatoCount;
     public int mushroomCount;
+    public int zucchiniCount;
+    public int onionCount;
+    public int cucumberCount;
+    public int garlicCount;
+    public int gingerCount;
+    public int pepperCount;
 
-    public bool stoveOn = false;
+    
 
     public float seconds, minutes;
     public float bonusSeconds;
+
+    
+    public bool stoveOn;
     // Start is called before the first frame update
     void Start()
     {
+        stoveOn = false; 
         bonusSeconds = 0;
         clockText = GameObject.Find("clock").GetComponent<Text>();
         recipeTitle = GameObject.Find("recipeTitle").GetComponent<Text>();
@@ -59,10 +69,16 @@ public class game : MonoBehaviour
         carrotCount = 0;
         mushroomCount = 0;
         potatoCount = 0;
-        
-        recipeChoice = 1;
+        zucchiniCount = 0;
+        onionCount = 0;
+        cucumberCount = 0;
+        garlicCount = 0;
+        gingerCount = 0;
+        pepperCount = 0;
 
-        if(recipeChoice == 0)
+    //recipeChoice = 1;
+        recipeChoice = global.recipeChoice;
+        if (recipeChoice == 0)
         {
             recipeTitle.text = "Custom Recipe";
             foodText1.text = "Put an Ingredient in the pot to start!";
@@ -78,26 +94,26 @@ public class game : MonoBehaviour
             Debug.Log("recipe1 chosen");
             //recipetitleO.GetComponent<Text>().text = "Tomato and Carrot Soup";
             recipeTitle.text = "Sunrise Soup";
-            food1Name = "Tomato";
-            food1Tot = 2;
-            food2Name = "Carrot";
-            food2Tot = 2;
+            food1Name = "tomato";
+            food1Tot = 3;
+            food2Name = "carrot";
+            food2Tot = 3;
         }
         if (recipeChoice == 2)
         {
             recipeTitle.text = "Tasty and Pasty";
             food1Name = "potato";
-            food1Tot = 2;
+            food1Tot = 3;
             food2Name = "broccoli";
-            food2Tot = 2;
+            food2Tot = 3;
         }
         if (recipeChoice == 3)
         {
             recipeTitle.text = "Two Veggie Soup";
             food1Name = "carrot";
-            food1Tot = 2;
+            food1Tot = 3;
             food2Name = "broccoli";
-            food2Tot = 2;
+            food2Tot = 3;
         }
         //med
         if(recipeChoice == 11)
@@ -108,7 +124,7 @@ public class game : MonoBehaviour
             food2Name = "onion";
             food2Tot = 2;
             food3Name = "potato";
-            food3Tot = 2;
+            food3Tot = 4;
             
         }
         //hard
@@ -116,11 +132,11 @@ public class game : MonoBehaviour
         {
             recipeTitle.text = "Mushroom Mumbo";
             food1Name = "mushroom";
-            food1Tot = 3;
+            food1Tot = 4;
             food2Name = "tomato";
-            food2Tot = 2;
+            food2Tot = 3;
             food3Name = "garlic";
-            food3Tot = 2;
+            food3Tot = 3;
             food3Name = "onion";
             food3Tot = 2;
         }
@@ -153,6 +169,10 @@ public class game : MonoBehaviour
         }
         clockText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
 
+        if (!stoveOn)
+        {
+            messageText.text = "Turn the stove on with the right knob";
+        }
     }
 
     
@@ -175,9 +195,37 @@ public class game : MonoBehaviour
         {
             mushroomCount++;
         }
-        if(food != food1Name && food != food2Name && food != food3Name && food != food4Name)//food4
+        if (food == "cucumber")
         {
-            if (recipeChoice == 1)
+            cucumberCount++;
+        }
+        if (food == "garlic")
+        {
+            garlicCount++;
+        }
+        if (food == "onion")
+        {
+            onionCount++;
+        }
+        if (food == "cucumber")
+        {
+            cucumberCount++;
+        }
+        if (food == "zucchini")
+        {
+            zucchiniCount++;
+        }
+        if (food == "ginger")
+        {
+            gingerCount++;
+        }
+        if (food == "pepper")
+        {
+            pepperCount++;
+        }
+        if (food != food1Name && food != food2Name && food != food3Name && food != food4Name)//food4
+        {
+            if (recipeChoice == 0)
             {
                 addCustomFood(food);
             }
@@ -196,7 +244,7 @@ public class game : MonoBehaviour
     }
     public void updateRecipeDisplay()
     {
-        if (recipeChoice == 1)
+        if (recipeChoice == 0)
         {
             if (food1Name != "")
             {
@@ -251,6 +299,34 @@ public class game : MonoBehaviour
         {
             return potatoCount;
         }
+        if (food == "cucumber")
+        {
+            return cucumberCount;
+        }
+        if (food == "garlic")
+        {
+            return garlicCount;
+        }
+        if (food == "onion")
+        {
+            return onionCount;
+        }
+        if (food == "cucumber")
+        {
+            return cucumberCount;
+        }
+        if (food == "zucchini")
+        {
+            return zucchiniCount;
+        }
+        if (food == "ginger")
+        {
+            return gingerCount;
+        }
+        if (food == "pepper")
+        {
+            return pepperCount;
+        }
         return 0;
     }
 
@@ -272,17 +348,23 @@ public class game : MonoBehaviour
         else if (food4Name == "")
         {
             food4Name = food;
-            messageText.text = "Maximum ingredients reached";
+            messageText.text = "";
             return;
         }
         else
         {
-            Debug.Log("maximum custom foods already");
+            messageText.text = "Added " + food + " to Soup";
+            //Debug.Log("maximum custom foods already");
         }
 
     }
     void addTime()
     {
         bonusSeconds += 10;
+    }
+
+    public void setMessage(string message)
+    {
+        messageText.text = message;
     }
 }
